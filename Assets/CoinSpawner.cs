e4da000f963caad218a5fb39773b23a3e1a57df5
davidgd72k <davidgd72k@gmail.com>
2026-06-03 01:25:19 +0200

    Ahora hay monedas y su generador

---
+++ Assets/CoinSpawner.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class CoinSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject coinPrefab;
+
+    [SerializeField] private float minSpawnTime = 1f;
+    [SerializeField] private float maxSpawnTime = 3f;
+
+    [SerializeField] private float minY = -2f;
+    [SerializeField] private float maxY = 2f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        // Empieza invocando una moneda.
+        Invoke(nameof(SpawnCoin), Random.Range(minSpawnTime, maxSpawnTime));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void SpawnCoin()
+    {
+        // Fija la posición de la moneda y la crea.
+        Vector3 spawnPos = new Vector3(
+            transform.position.x,
+            Random.Range(minY, maxY),
+            0f
+        );
+
+        Instantiate(coinPrefab, spawnPos, Quaternion.identity);
+
+        // Vuelve a llamarse a sí mismo para repetir este proceso.
+        Invoke(nameof(SpawnCoin), Random.Range(minSpawnTime, maxSpawnTime));
+    }
+}
